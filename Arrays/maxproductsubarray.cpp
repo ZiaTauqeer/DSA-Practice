d1e40969d436c3006fd5d3e166f81628888ca06c
ZiaTauqeer <tauqeerzia2024@gmail.com>
2026-08-19 05:18:07 +0530

    Added Max product subarray

---
+++ Arrays/maxproductsubarray.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxProduct(vector<int>& nums) {
+
+    int pre = 1;
+    int suff = 1;
+    int ans = INT_MIN;
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++) {
+
+        if (pre == 0)
+            pre = 1;
+
+        if (suff == 0)
+            suff = 1;
+
+        pre = pre * nums[i];
+        suff = suff * nums[n - i - 1];
+
+        ans = max(ans, max(pre, suff));
+    }
+
+    return ans;
+}
+
+int main() {
+
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int result = maxProduct(nums);
+
+    cout << "Maximum product subarray is: " << result << endl;
+
+    return 0;
+}
