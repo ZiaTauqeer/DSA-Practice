8cc6bc080533e3e81db8bfac2605eae6dc1f2281
ZiaTauqeer <tauqeerzia2024@gmail.com>
2026-08-20 02:55:17 +0530

    Added Binary search lower bound

---
+++ BinarySearch/lowerbound.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int>& arr, int target) {
+
+    int n = arr.size();
+
+    int low = 0;
+    int high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+
+        int mid = (low + high) / 2;
+
+        if (arr[mid] >= target) {
+            ans = mid;
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+
+    int n;
+
+    cout << "Enter size of sorted array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter elements in sorted order: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int target;
+
+    cout << "Enter target: ";
+    cin >> target;
+
+    int result = lowerBound(arr, target);
+
+    if (result == n) {
+        cout << "No element greater than or equal to " << target << " exists." << endl;
+    }
+    else {
+        cout << "Lower bound of " << target << " is at index: "
+             << result << endl;
+
+        cout << "Element at lower bound: " << arr[result] << endl;
+    }
+
+    return 0;
+}
